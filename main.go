14e0f9e5f6d3c0bfc6dd2737ed44c923508798b5
Samiul Islam <44663019+samiul16@users.noreply.github.com>
2025-06-03 16:53:48 +0600

    Added form handler and functions.

--- main.go
+++ main.go
@@ -5,10 +5,31 @@ import (
 	"net/http"
 )
 
+func helloHandlerFunc (w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Mrthod Not Supported", http.StatusNotFound)
+	}
+
+	fmt.Fprintf(w, "Hello")
+}
+
+func formHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Parse form error: %v", err)
+		return 
+	}
+}
+
 func main() {
 	fmt.Println("++++ Starting Server ++++")
 	fileHandler := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileHandler)
+	http.HandleFunc("/hello",helloHandlerFunc)
+	http.HandleFunc("/form", formHandlerFunc)
 
 	fmt.Println("Server running at PORT: 8080")
 
